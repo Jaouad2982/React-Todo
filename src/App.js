947dfa07de613b2234c4cb39b61b678cc456f29e
Jaouad Sarouti <jadsarouti@outlook.com>
2019-01-23 22:59:40 -0500

    Create Static Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from 'react';
-
+import React from "react"
+import Header from "./components/Header";
 class App extends React.Component {
-  // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
-  render() {
-    return (
-      <div>
-        <h2>Welcome to your Todo App!</h2>
-      </div>
-    );
-  }
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return (
+            <div>
+                   <Header />
+            </div>
+        )
+    }
 }
 
-export default App;
+export default App
---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+
+const Header =()=>{
+    return (
+        <div>
+                    <h3>This is a header</h3>
+        </div>
+    )
+}
+
+export default Header
